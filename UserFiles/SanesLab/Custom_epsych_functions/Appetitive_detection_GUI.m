9885efe0784e6b22355deb15c83eb431135b80dc
SanesLab <saneslab_github@cns.nyu.edu>
2015-10-01 12:22:23 -0400

    Piloting light cues Addition of common average referencing for multichannel signals

--- UserFiles/SanesLab/Custom_epsych_functions/Appetitive_detection_GUI.m
+++ UserFiles/SanesLab/Custom_epsych_functions/Appetitive_detection_GUI.m
@@ -26,7 +26,7 @@ end
 
 %SET UP INITIAL GUI TEXT BEFORE GUI IS MADE VISIBLE
 function Appetitive_detection_GUI_OpeningFcn(hObject, ~, handles, varargin)
-global ROVED_PARAMS GUI_HANDLES CONFIG RUNTIME PERSIST
+global AX ROVED_PARAMS GUI_HANDLES CONFIG RUNTIME PERSIST
 
 %Start fresh
 GUI_HANDLES = [];
@@ -40,6 +40,24 @@ if RUNTIME.UseOpenEx
     handles.dev = 2;
 else
     handles.dev = 1;
+end
+
+
+%If we're using OpenEx, 
+if RUNTIME.UseOpenEx
+   
+    %Create initial, non-biased weights
+    v = ones(1,16);
+    WeightMatrix = diag(v);
+    
+    %Reshape matrix into single row for RPVds compatibility
+    WeightMatrix =  reshape(WeightMatrix',[],1);
+    WeightMatrix = WeightMatrix';
+    
+    AX.WriteTargetVEX('Phys.WeightMatrix',0,'F32',WeightMatrix);
+    
+    %Remove correlated noise signals from channels
+    referenceChannels
 end
 
 
@@ -1114,6 +1132,8 @@ switch tag
 end
 
 
+
+
 %----------------------------------------------------------------------
 %%%%%%%%%%%%%%%%%%    TEXT UPDATE FUNCTIONS   %%%%%%%%%%%%%%%%%%%%%%
 %----------------------------------------------------------------------
@@ -1445,11 +1465,6 @@ set(h.micAx,'ylim',[0 10]);
 set(h.micAx,'xlim',[0 2]);
 set(h.micAx,'XTickLabel','');
 ylabel(h.micAx,'RMS voltage','fontname','arial','fontsize',12)
-
-
-
-
-
 
 
 
@@ -1854,17 +1869,100 @@ end
 
 
 
+%-----------------------------------------------------------
+%%%%%%%%%%%%%%       PHYSIOLOGY FUNCTIONS     %%%%%%%%%%%%%%%
+%------------------------------------------------------------
+
+function referenceChannels
+global AX
+%The method we're using here to reference channels is the following:
+%First, bad channels are removed.
+%Second a single channel is selected and held aside.
+%Third, all of the remaining (good, non-selected) channels are averaged.
+%Fourth, this average is subtracted from the selected channel.
+%This process is repeated for each good channel.
+%
+%The way this method is implemented in the RPVds circuit is as follows:  
+%
+%From Brad Buran:
+%
+% This is implemented using matrix multiplication in the format D x C =
+% R. C is a single time-slice of data in the shape [16 x 1]. In other
+% words, it is the value from all 16 channels sampled at a single point
+% in time. D is a 16 x 16 matrix. R is the referenced output in the
+% shape [16 x 1]. Each row in the matrix defines the weights of the
+% individual channels. So, if you were averaging together channels 2-16
+% and subtracting the mean from the first channel, the first row would
+% contain the weights:
+% 
+% [1 -1/15 -1/15 ... -1/15]
+% 
+% If you were averaging together channels 2-8 and subtracting the mean
+% from the first channel:
+% 
+% [1 -1/7 -1/7 ... -1/7 0 0 0 ... 0]
+% 
+% If you were averaging together channels 3-8 (because channel 2 was
+% bad) and subtracting the mean from the first channel:
+% 
+% [1 0 -1/6 ... -1/6 0 0 0 ... 0]
+% 
+% To average channels 1-4 and subtract the mean from the first channel:
+% 
+% [3/4 -1/4 -1/4 -1/4 0 ... 0]
+% 
+% To repeat the same process (average channels 1-4 and subtract the
+% mean) for the second channel, the second row in the matrix would be:
+% 
+% [-1/4 3/4 -1/4 -1/4 0 ... 0]
 
 
 
 
+%Hard coded for a 16 channel array
+numchannels = 16;
+
+%Prompt user to identify bad channels
+channelList = {'1','2','3','4','5','6','7','8',...
+    '9','10','11','12','13','14','15','16'};
+
+header = 'Select bad channels. Hold Cntrl to select multiple channels.';
+
+bad_channels = listdlg('ListString',channelList,'InitialValue',8,...
+    'Name','Channels','PromptString',header,...
+    'SelectionMode','multiple','ListSize',[300,300])
 
 
-
-
-
-
-
-
-
+if ~isempty(bad_channels)
+    %Calculate weight for non-identical pairs
+    weight = -1/(numchannels - numel(bad_channels) - 1);
+    
+    %Initialize weight matrix
+    WeightMatrix = repmat(weight,numchannels,numchannels);
+    
+    %The weights of all bad channels are 0.
+    WeightMatrix(:,bad_channels) = 0;
+    
+    %Do not perform averaging on bad channels: leave as is.
+    WeightMatrix(bad_channels,:) = 0;
+    
+    %For each channel
+    for i = 1:numchannels
+        
+        %Its own weight is 1
+        WeightMatrix(i,i) = 1;
+        
+    end
+    
+    
+    
+    %Reshape matrix into single row for RPVds compatibility
+    WeightMatrix =  reshape(WeightMatrix',[],1);
+    WeightMatrix = WeightMatrix';
+    
+    
+    %Send to RPVds
+    AX.WriteTargetVEX('Phys.WeightMatrix',0,'F32',WeightMatrix);
+    %verify = AX.ReadTargetVEX('Phys.WeightMatrix',0, 256,'F32','F64');
+end
 
