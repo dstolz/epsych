94b14a0b75fbeffed07e5d163506e12adf003cef
SanesLab <saneslab_github@cns.nyu.edu>
2015-12-08 16:21:57 -0500

    New TDT2MAT and SEV2MAT functions for MATLAB analysis of TDT Tank data

--- UserFiles/SanesLab/DataAnalysis/TankAnalysis.m
+++ UserFiles/SanesLab/DataAnalysis/TankAnalysis.m
@@ -1,21 +1,31 @@
 %% TANK ANALYSIS TEST: SANESLAB
 
-tank = 'C:\TANKS\PhysTest'; % Specify tank name
-block = 'Block-2'; % Specify block name
+tank = 'C:\TDT\OpenEx\MyProjects\MLC\ID_223649\ID_223649'; % Specify tank name
+block = 'Block-1'; % Specify block name
 
-%Retrieve stimulus epochs (2) and stream data (4) from tank
-data = TDT2mat(tank,block,'type',[2 4]);
+%Retrieve data
+data = TDT2mat(tank,block)';
+
+ch = 1;
+
+%data.streams.Wave.data contains the raw physiology data.
+%Each column = 1 channel
+%Each row = 1 timestamp
+samples = size(data.streams.Wave.data,2);
+x = 1:samples;
+x = x/data.streams.Wave.fs;
+
+figure
+plot(x,data.streams.Wave.data(ch,:))
 
 
-%data.streams.Wave.data contains the raw physiology data. 
-    %Each column = 1 channel
-    %Each row = 1 timestamp
-    samples = size(data.streams.Wave.data,1);
-    secs = samples/data.streams.Wave.fs;
-    
+
+
+
+
+
 %[Trial onset time, Trial offset time, trial type]
 ttyp = [data.epocs.TTyp.onset,data.epocs.TTyp.offset,data.epocs.TTyp.data];
-
 
 %[Trial offset time, response code]
 rcod = [data.epocs.RCod.onset,data.epocs.RCod.data];
