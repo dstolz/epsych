c3b5faf1232483f671c6fdce37d653e408c73114
dstolz <daniel.stolzberg@gmail.com>
2015-12-16 18:11:07 -0500

    renamed global variable from ExptVerbosity to GVerbosity

--- helpers/vprintf.m
+++ helpers/vprintf.m
@@ -2,13 +2,13 @@ function vprintf(verbose_level,varargin)
 % vprintf(verbose_level,[red],msg,[moreinputs])
 %
 % Prints timestamp and text to the command window based on the current
-% value of the global variable ExptVerbosity.  ExptVerbosity is a scalar
+% value of the global variable GVerbosity.  GVerbosity is a scalar
 % integer value between -1 and 3:
 %  -1 log message, but do not print to screen
 %   0 suppresses nearly all non-critcal messages
-%   1 low, information that may be generally useful to user (default)
+%   1 low, information that may be generally useful to user
 %   2 medium, information that can be helpful for debugging
-%   3 high, lots of information about nearly all processes
+%   3 high, lots of information about nearly all processes (debugging)
 %
 % Uses fprintf to print text. Additonal values must correspond to the
 % escape characters defined as if calling fprintf directly.
@@ -16,30 +16,40 @@ function vprintf(verbose_level,varargin)
 % This function always prints a '\n' character at the end of the line,
 % skipping a line.
 %
-% This function also prints messages at verbose_level <= ExptVerbosity to a
+% This function also prints messages at verbose_level <= GVerbosity to a
 % log file for debugging purposes.  Each message in the log will also
 % contain the function and line number sending the message.  Note that
-% logging at verbose level 3 will probably screw up timing slightly and
-% should only be used for debugging.  A new log will be automatically
-% generated for each day this script is called.
+% logging at verbose level 3 may probably screw up critical timing slightly
+% and should only be used for debugging code.  A new log will be
+% automatically generated for each day this script is called in a
+% subdirectory to the current working directory called 'logs'.
 %
 % ex:  
-%      global ExptVerbosity
-%      ExptVerbosity = 2;
+%      global GVerbosity
+%      GVerbosity = 2;
 %      vprintf(2,'This is a level %d message: %s',2,'medium verbosity')
 %      18:51:35.958: This is a level 2 message: medium verbosity
-%      
+% 
+%      vprintf(3,'This message will not be printed because GVerbosity = %d',GVerbosity)
+%      18:51:35.958: This is a level 2 message: medium verbosity
+% 
 %      vprintf(1,1,'This is a level %d message: %s',1,'low verbosity')
 %      18:51:35.958: This is a level 1 message: low verbosity
 %
 % 
+% It is probably a good idea to close the log file when done.
+%       global GLogFID % file id of the log
+%       if ~isempty(GLogFID) && GLogFID >2
+%           fclose(GLogFID); 
+%       end
+%
 % Daniel.Stolzberg@gmail.com 2015
 
-global ExptVerbosity
+global GVerbosity
 
-if isempty(ExptVerbosity), ExptVerbosity = 1; end
+if isempty(GVerbosity) || ~isnumeric(GVerbosity), GVerbosity = 1; end
 
-if verbose_level > ExptVerbosity, return; end
+if verbose_level > GVerbosity, return; end
 
 
 
@@ -101,28 +111,27 @@ end
 
 
 
-
 function logmessage(msg,curTimeStr,moreinputs)
 % Print to log file
-global ExptLogFID
+global GLogFID
 
 try
-    ftell(ExptLogFID);
+    ftell(GLogFID);
     needNewLog = false;
 catch %#ok<CTCH>
     needNewLog = true;
 end
 
-if needNewLog || isempty(ExptLogFID) || ExptLogFID == -1
-    ExptLogFID = fopen(sprintf('logs\\expt_log_%s.log',datestr(now,'ddmmmyyyy')),'at');
+if needNewLog || isempty(GLogFID) || GLogFID == -1
+    GLogFID = fopen(sprintf('logs\\expt_log_%s.log',datestr(now,'ddmmmyyyy')),'at');
 end
 
-if isnumeric(ExptLogFID) && ExptLogFID > 2
+if isnumeric(GLogFID) && GLogFID > 2
     st = dbstack;
     if isempty(moreinputs)
-        fprintf(ExptLogFID,['%s,%s,%d: ' msg '\n'],curTimeStr,st(2).name,st(2).line);
+        fprintf(GLogFID,['%s,%s,%d: ' msg '\n'],curTimeStr,st(2).name,st(2).line);
     else
-        fprintf(ExptLogFID,['%s,%s,%d: ' msg '\n'],curTimeStr,st(2).name,st(2).line,moreinputs{:});
+        fprintf(GLogFID,['%s,%s,%d: ' msg '\n'],curTimeStr,st(2).name,st(2).line,moreinputs{:});
     end
 end
 
