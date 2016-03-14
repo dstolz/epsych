ad0efc80450ab79b60ce18645476ccd78794562e
dstolz <dstolzbe@uwo.ca>
2016-03-14 15:51:45 -0400

    Added option to specify sampling rate

--- TDTfun/TDT_SetupRP.m
+++ TDTfun/TDT_SetupRP.m
@@ -1,12 +1,12 @@
-function varargout = TDT_SetupRP(mod,modid,ct,rpfile)
-% RP = TDT_SetupRP(mod,modid,ct,rpfile)
+function varargout = TDT_SetupRP(mod,modid,ct,rpfile,Fs)
+% RP = TDT_SetupRP(mod,modid,ct,rpfile,[Fs])
 % [RP,status] = TDT_SetupRP( ...
-% 
+%
 % where mod is a valid module type: 'RZ5','RX6','RP2', etc..
 %   modid is the module id.  default is 1.
 %   ct is a connection type:  'GB' or 'USB'
 %   rpfile is the full path to an RPvds file.
-% 
+%
 % returns RP which is the handle to the RPco.x control.  Also returns
 % status which is a bitmask of the module status.  Use with bitget function
 %   Bit# 0 = Connected
@@ -14,9 +14,20 @@ function varargout = TDT_SetupRP(mod,modid,ct,rpfile)
 %   Bit# 2 = Circuit running
 %   Bit# 3 = RA16BA Battery
 % (see page 43 of ActiveX reference manual for more status values).
-% 
+%
+% Optionally specify the sampling frequency (Fs).  See "LoadCOFsf" in the
+% TDT ActiveX manual for more info.
+%   Fs = 0  for 6 kHz
+%   Fs = 1  for 12 kHz
+%   Fs = 2  for 25 kHz
+%   Fs = 3  for 50 kHz
+%   Fs = 4  for 100 kHz
+%   Fs = 5  for 200 kHz
+%   Fs = 6  for 400 kHz
+%   Fs > 50 for arbitrary sampling rates
+%
 % See also TDT_SetupDA, TDT_SetupTT, bitget
-% 
+%
 % DJS (c) 2010
 
 varargout = cell(1,3);
@@ -60,14 +71,14 @@ if ~eval(sprintf('RP.Connect%s(''%s'',%d)',mod,ct,modid))
 else
     fprintf('%s_%d connected ... ',mod,modid)
     RP.ClearCOF;
-    if ~RP.LoadCOF(rpfile)
-        beep
-        errordlg(sprintf(['Unable to load RPvds file to %s module!\n\n', ...
-            'The RPvds file exists, but can not be loaded for some reason'], ...
-            mod),'Loading Error','modal');
-        CloseUp(RP,h);
-        return
+    
+    if nargin == 5
+        e = RP.LoadCOFsf(rpfile,Fs);
     else
+        e = RP.LoadCOF(rpfile);
+    end
+    
+    if e  
         fprintf('loaded ...')
         if ~RP.Run
             beep
@@ -79,6 +90,14 @@ else
         else
             fprintf('running\n')
         end
+        
+    else
+        beep
+        errordlg(sprintf(['Unable to load RPvds file to %s module!\n\n', ...
+            'The RPvds file exists, but can not be loaded for some reason'], ...
+            mod),'Loading Error','modal');
+        CloseUp(RP,h);
+        return
     end
 end
 
