7b2bb3c9c0fdc1a8be360bac5c1454c0be7ef44a
dstolz <daniel.stolzberg@gmail.com>
2016-05-23 16:32:09 -0400

    preproc

--- UserFiles/LomberLab/preprocessing/Batch_offlineSpikeDetect.m
+++ UserFiles/LomberLab/preprocessing/Batch_offlineSpikeDetect.m
@@ -1,9 +1,18 @@
 %% Batch offlineSpikeDetect
 
-tank = 'D:\Data Tanks\CHRONIC_EPHYS_TANKS\HANSOLO_20160426';
+tank = 'D:\KingKong_Cloud (HIDEME)\BIG_DATA\ChronicEphys\HANSOLO\HANSOLO_20160519';
 blocks = TDT2mat(tank);
+minSpikesPerSecond = 1;
+
 
 %%
 for b = blocks
-    offlineSpikeDetect(tank,char(b),'D:\DataProcessing\HANSOLO\plxTEST');
+    fprintf('Processing ''%s'' of tank ''%s''\n',char(b),tank)
+    d = TDT2mat(tank,char(b),'NODATA',true,'VERBOSE',false);
+    d = datevec(d.info.duration,'HH:MM:SS');
+    minSpikes = (d(4)*3600+d(5)*60+d(6))*minSpikesPerSecond;
+    fprintf('Minimum Spikes: %d\n',minSpikes)
+    
+    offlineSpikeDetect(tank,char(b),'D:\DataProcessing\HANSOLO\HANSOLO_20160519\plx', ...
+        [],[],[],[],minSpikes);
 end
