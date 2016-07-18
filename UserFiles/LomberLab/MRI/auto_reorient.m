42cd0ab2e9f5b9f53c3b736c0da291cb58321da0
dstolz <daniel.stolzberg@gmail.com>
2016-07-18 09:07:47 -0400

    auto reorientation of MRI images (note: uses canonical avg152T1.nii)

---
+++ UserFiles/LomberLab/MRI/auto_reorient.m
@@ -0,0 +1,22 @@
+function auto_reorient(p)
+% automatically reorient MRI image
+% from: https://en.wikibooks.org/wiki/SPM/How-to
+
+if ~nargin
+    [p,sts] = spm_select(Inf,'image');
+    if ~sts, return; end
+end
+p = cellstr(p);
+vg = spm_vol(fullfile(spm('Dir'),'canonical','avg152T1.nii'));
+tmp = [tempname '.nii'];
+for i=1:numel(p)
+    spm_smooth(p{i},tmp,[4 4 4]);
+    vf = spm_vol(tmp);
+    M  = spm_affreg(vg,vf,struct('regtype','rigid'));
+    [u,s,v] = svd(M(1:3,1:3));
+    M(1:3,1:3) = u*v';
+    N  = nifti(p{i});
+    N.mat = M*N.mat;
+    create(N);
+end
+spm_unlink(tmp);
