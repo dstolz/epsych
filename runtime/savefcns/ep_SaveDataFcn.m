fba882c8e00297a62285ae5197cab45a68c96436
dstolz <daniel.stolzberg@gmail.com>
2016-08-24 22:54:57 -0400

    Update to comments

--- runtime/savefcns/ep_SaveDataFcn.m
+++ runtime/savefcns/ep_SaveDataFcn.m
@@ -3,7 +3,7 @@ function ep_SaveDataFcn(RUNTIME)
 % 
 % Default function fo saving behavioral data
 % 
-% Use ep_PsychConfig GUI to specify custom function.
+% Use ep_RunExpt GUI to specify custom function.
 % 
 % Daniel.Stolzberg@gmail.com 2014
 
